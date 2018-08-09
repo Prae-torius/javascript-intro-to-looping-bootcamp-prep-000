98e0e5972338c719b3a99ded1ab08d99f7d3799e
Preston A Arnette <arnettepreston@gmail.com>
2018-08-09 05:57:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,11 +3,19 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
       array.push(`I am ${i} strange loop.`);
+<<<<<<< HEAD
     } else {
       array.push(`I am ${i} strange loops.`);
     }
   }
   
+=======
+    }
+    else {
+      array.push(`I am ${i} strange loops.`);
+    }
+  }
+>>>>>>> bc1e428ec168c8bea89e3bdffba43db8093819a6
   return array;
 }
    
@@ -17,6 +25,7 @@ function whileLoop(n) {
   while (n > 0) {
     console.log(--n);
   }
+<<<<<<< HEAD
   return 'done';
 }
 
@@ -34,3 +43,19 @@ function doWhileLoop(array) {
   
   return array;
 }
+=======
+  return ('done');
+}
+
+
+function doWhileLoop(array){
+  var i = 1;
+    function incrementVariable() {
+      i = i + 1;
+    }
+  do { 
+    array.pop();
+  } while (array.length > 0 && incrementVariable());
+  return array;
+}
+>>>>>>> bc1e428ec168c8bea89e3bdffba43db8093819a6
